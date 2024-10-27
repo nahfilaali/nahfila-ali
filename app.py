efb77b6ee382ccd72654d0280ab8c195f8d37269
nahfilaali <nahfila.ali05@myhunter.cuny.edu>
2024-10-27 16:25:46 -0400

    updated font, added 2 new projects

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html') 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html') 
 
 @app.route('/resume')
 def resume():
@@ -23,4 +23,4 @@ def projects():
     return render_template('projects.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
